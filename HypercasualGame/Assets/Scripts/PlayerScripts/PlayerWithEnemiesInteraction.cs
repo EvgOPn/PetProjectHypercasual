35f20b10f9385385d7bf31230cf65ffa0f55f89d
Evg Pn <ye.ponomarenko@gmail.com>
2020-09-24 20:29:26 +0300

    feat: add player/enemies interaction
    -add test score counter

---
+++ HypercasualGame/Assets/Scripts/PlayerScripts/PlayerWithEnemiesInteraction.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerWithEnemiesInteraction : MonoBehaviour
+{
+	[SerializeField] private string _simpleEnemyTag = "";
+	[SerializeField] private string _targetEnemyTag = "";
+
+	public UnityEvent OnSimpleEnemyTriggered;
+	public UnityEvent OnTargetEnemyTriggered;
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.gameObject.CompareTag(_simpleEnemyTag))
+		{
+			OnSimpleEnemyTriggered?.Invoke();
+		}
+		else if (collision.gameObject.CompareTag(_targetEnemyTag))
+		{
+			Destroy(collision.gameObject);
+			OnTargetEnemyTriggered?.Invoke();
+		}
+	}
+}
